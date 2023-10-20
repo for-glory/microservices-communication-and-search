6e3c3058684838a16c68d076bc4e76dee5d66189
for-glory <forglory0905@outlook.com>
2023-10-20 20:31:19 -0300

    update configs

--- ms-ad-api/config/queue.php
+++ ms-ad-api/config/queue.php
@@ -71,6 +71,17 @@ return [
             'after_commit' => false,
         ],
 
+        'rabbitmq' => [
+            'driver' => 'rabbitmq',
+            'queue' => env('RABBITMQ_QUEUE', 'default'),
+            'host' => env('RABBITMQ_HOST', '127.0.0.1'),
+            'port' => env('RABBITMQ_PORT', 5672),
+            'vhost' => env('RABBITMQ_VHOST', '/'),
+            'login' => env('RABBITMQ_LOGIN', 'guest'),
+            'password' => env('RABBITMQ_PASSWORD', 'guest'),
+            'queue' => env('RABBITMQ_QUEUE', 'default'),
+        ],
+
     ],
 
     /*
