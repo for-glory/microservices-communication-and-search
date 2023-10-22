b576339e69b736e07f21c12e8b0262cb4d5770e8
for-glory <forglory0905@outlook.com>
2023-10-21 23:08:41 -0300

    feat: add ms-ad-api user creation with event queue rabbitmq sending

--- ms-ad-api/config/queue.php
+++ ms-ad-api/config/queue.php
@@ -77,9 +77,8 @@ return [
             'host' => env('RABBITMQ_HOST', '127.0.0.1'),
             'port' => env('RABBITMQ_PORT', 5672),
             'vhost' => env('RABBITMQ_VHOST', '/'),
-            'login' => env('RABBITMQ_LOGIN', 'guest'),
+            'user' => env('RABBITMQ_USER', 'guest'),
             'password' => env('RABBITMQ_PASSWORD', 'guest'),
-            'queue' => env('RABBITMQ_QUEUE', 'default'),
         ],
 
     ],
